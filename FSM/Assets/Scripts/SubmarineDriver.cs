46e376713b751d967961d3c17ee7b0fb995c3934
Kindon Smith <kindonsmith@gmail.com>
2022-03-02 16:50:44 -0800

    Added SubmarineDriver file
    -Updated Development_Kindon Scene
    -Added move-to-click functionality for submarine script

--- FSM/Assets/Scripts/SubmarineDriver.cs
+++ FSM/Assets/Scripts/SubmarineDriver.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SubmarineDriver : MonoBehaviour
 {
@@ -9,10 +10,21 @@ public class SubmarineDriver : MonoBehaviour
     {
         
     }
+    public Camera cam;
+    public NavMeshAgent agent;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
     }
 }
