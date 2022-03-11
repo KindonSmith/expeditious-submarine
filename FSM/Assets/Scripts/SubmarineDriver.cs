061d35172b06b40be4e1f4355c93512f734bab6d
Kindon Smith <kindonsmith@gmail.com>
2022-03-10 16:34:54 -0800

    SubmarineDriver script
    Updated SubmarineDriver.cs file to include raycast target finding with layermask

--- FSM/Assets/Scripts/SubmarineDriver.cs
+++ FSM/Assets/Scripts/SubmarineDriver.cs
@@ -5,6 +5,9 @@ using UnityEngine.AI;
 
 public class SubmarineDriver : MonoBehaviour
 {
+
+    public float detectionRadius = 10f;
+    [SerializeField] LayerMask lm;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +19,31 @@ public class SubmarineDriver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Ray areaRay = new Ray(transform.position, transform.forward);
+        for(int i = -1; i < 2; i++)
+        {
+            for(int j = -1; j < 2; j++)
+            {
+                var vec = new Vector3(i, 0, j);
+                areaRay = new Ray(transform.position, vec);
+                if (Physics.Raycast(areaRay, out RaycastHit hitinfo, detectionRadius, lm))
+                {
+                    agent.SetDestination(hitinfo.point);
+                    /*
+                    Debug.DrawRay(transform.position, vec * hitinfo.distance, Color.green);
+                    Debug.Log("Hit");*/
+
+                }
+                else
+                {
+                    /*
+                    Debug.DrawRay(transform.position, vec * 20f, Color.red);
+                    */
+                }
+            }
+        }
+        /*
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray = cam.ScreenPointToRay(Input.mousePosition);
@@ -25,6 +53,6 @@ public class SubmarineDriver : MonoBehaviour
             {
                 agent.SetDestination(hit.point);
             }
-        }
+        }*/
     }
 }
