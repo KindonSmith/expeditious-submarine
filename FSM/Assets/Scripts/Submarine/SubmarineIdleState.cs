43be2cfc96f5490291797c151d91258c6effd6a0
Kindon Smith <kindonsmith@gmail.com>
2022-03-17 11:14:48 -0700

    Added back click-to-move Submarine controls in Sub idle state

--- FSM/Assets/Scripts/Submarine/SubmarineIdleState.cs
+++ FSM/Assets/Scripts/Submarine/SubmarineIdleState.cs
@@ -1,8 +1,24 @@
 using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.AI;
 
 public class SubmarineIdleState : SubmarineBaseState
 {
-    public override void EnterState(SubmarineStateManager submarine) { }
-    public override void UpdateState(SubmarineStateManager submarine) { }
+    public override void EnterState(SubmarineStateManager submarine) {
+    }
+    public override void UpdateState(SubmarineStateManager submarine) {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = submarine.cam.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out hit))
+            {
+                submarine.agent.SetDestination(hit.point);
+            }
+        }
+
+    }
     public override void OnCollisionEnter(SubmarineStateManager submarine) { }
+
 }
--- FSM/Assets/Scripts/Submarine/SubmarineStateManager.cs
+++ FSM/Assets/Scripts/Submarine/SubmarineStateManager.cs
@@ -6,16 +6,17 @@ using UnityEngine.AI;
 public class SubmarineStateManager : MonoBehaviour
 {
     // Start is called before the first frame update
-
-    SubmarineBaseState CurrentState;
+    
+    public SubmarineBaseState CurrentState;
     SubmarineHuntingState HuntingState = new SubmarineHuntingState();
     SubmarineIdleState IdleState = new SubmarineIdleState();
     public int detectionRadius = 5;
     public LayerMask lm;
     public NavMeshAgent agent;
+    public Camera cam;
     void Start()
     {
-        CurrentState = HuntingState;
+        CurrentState = IdleState;
         CurrentState.EnterState(this);
     }
 
